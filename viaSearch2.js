240f711b09d5fc0b03994b44b7666d015c2181bb
lcreed <laura.creed@gmail.com>
2023-02-21 12:49:20 -0600

    still playing with header search and notifications

--- viaSearch2.js
+++ viaSearch2.js
@@ -1,9 +1,12 @@
-function searchEmails() {
+function searchEmailsAndSendResults() {
     // Set the search strings you want to look for
     var searchStrings = ["jobopportunityforyou", "ceipal","bullhornmail","jobdiva"];
-       
+    
     // Search for emails received in the last 7 days
     var threads = GmailApp.search('newer_than:7d');
+    
+    // Initialize an empty results string
+    var results = "";
     
     // Loop through each thread
     for (var i = 0; i < threads.length; i++) {
@@ -20,16 +23,22 @@ function searchEmails() {
         for (var k = 0; k < searchStrings.length; k++) {
           var searchString = searchStrings[k];
           
-          // If the search string is found in the raw content of the message, log the sending email domain and subject line
+          // If the search string is found in the raw content of the message, add the sender email address and subject line to the results string
           if (rawContent.indexOf(searchString) !== -1) {
             var from = message.getFrom();
-            var tome = message.getTo();
             var subject = message.getSubject();
-            Logger.log(tome + " Email address: " + from + ", Subject: " + subject);
+            results += "Email address: " + from + ", Subject: " + subject + "\n";
             break;
-            // should also add new label to the mail from the vias so they can be easily spotted
           }
         }
       }
+    }
+    
+    // If results were found, send an email to the recipient containing the results
+    if (results) {
+      var recipient = "laura.creed@gmail.com"; // Replace with the email address of the recipient
+      var subject = "Search Results";
+      var body = "The following results were found:\n\n" + results;
+      GmailApp.sendEmail(recipient, subject, body);
     }
   }
